8e4507924ef2493594abfe47adee740cdc06bfd5
Moon Geonwoo <moongunwoo7019@gmail.com>
2024-05-14 18:36:23 +0900

    Fixed detail medicine model

--- medicines/models.py
+++ medicines/models.py
@@ -6,6 +6,35 @@ class Medicine(CommonModel):
     name = models.CharField(
         max_length=150,
     )
+    serial_number = models.PositiveBigIntegerField()
+    efficacy = models.TextField(
+        default="",
+        blank=True,
+    )
+    usage = models.TextField(
+        default="",
+        blank=True,
+    )
+    need_to_know = models.TextField(
+        default="",
+        blank=True,
+    )
+    cautions = models.TextField(
+        default="",
+        blank=True,
+    )
+    beware_food = models.TextField(
+        default="",
+        blank=True,
+    )
+    side_effect = models.TextField(
+        default="",
+        blank=True,
+    )
+    how_to_store = models.TextField(
+        default="",
+        blank=True,
+    )
 
     def __str__(self) -> str:
         return self.name
