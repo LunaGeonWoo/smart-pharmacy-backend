5b76722b396afc46a0401a93c5c8a0afd833f980
Moon Geonwoo <moongunwoo7019@gmail.com>
2024-05-15 15:14:59 +0900

    fix: Medicine serial_number=True

--- medicines/models.py
+++ medicines/models.py
@@ -4,46 +4,57 @@ from common.models import CommonModel
 
 class Medicine(CommonModel):
     serial_number = models.PositiveBigIntegerField(
-        unique=0,
+        unique=True,
+        verbose_name="품목일련번호",
     )
     name = models.CharField(
         max_length=150,
+        verbose_name="제품명",
     )
     company = models.CharField(
         default="",
         max_length=150,
+        verbose_name="업체명",
     )
     main_ingredient = models.TextField(
         default="",
         blank=True,
+        verbose_name="주성분",
     )
     efficacy = models.TextField(
         default="",
         blank=True,
+        verbose_name="이 약의 효능은 무엇입니까?",
     )
     usage = models.TextField(
         default="",
         blank=True,
+        verbose_name="이 약은 어떻게 사용합니까?",
     )
     need_to_know = models.TextField(
         default="",
         blank=True,
+        verbose_name="이 약을 사용하기 전에 반드시 알아야 할 내용은 무엇입니까?",
     )
     cautions = models.TextField(
         default="",
         blank=True,
+        verbose_name="이 약의 사용상 주의사항은 무엇입니까?",
     )
     beware_food = models.TextField(
         default="",
         blank=True,
+        verbose_name="이 약을 사용하는 동안 주의해야 할 약 또는 음식은 무엇입니까?",
     )
     side_effect = models.TextField(
         default="",
         blank=True,
+        verbose_name="이 약은 어떤 이상반응이 나타날 수 있습니까?",
     )
     how_to_store = models.TextField(
         default="",
         blank=True,
+        verbose_name="이 약은 어떻게 보관해야 합니까?",
     )
 
     def __str__(self) -> str:
