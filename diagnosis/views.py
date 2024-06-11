c5145d91b120f8db8f9c4a30d0a2269d5fbef176
Happy <donghoikoo6@gmail.com>
2024-06-11 10:52:06 +0900

    api.py views.py merge

--- diagnosis/views.py
+++ diagnosis/views.py
@@ -2,8 +2,34 @@ from django.conf import settings
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-import requests
 from openai import OpenAI
+
+
+class GPT4o:
+    client = OpenAI()
+    messages = [
+        {
+            "role": "system",
+            "content": "You're a pharmacist who's there to help sick patients and recommend medications.",
+        },
+    ]
+
+    def reset(self):
+        self.messages = [
+            {
+                "role": "system",
+                "content": "You're a pharmacist who's there to help sick patients and recommend medications.",
+            },
+        ]
+        completion = self.client.chat.completions.create(
+            model="gpt-4o",
+            messages=[
+                {
+                    "role": "user",
+                    "content": "Write the symptoms you are experiencing",
+                },
+            ],
+        )
 
 
 class Diagnosis(APIView):
