e77ee74f9d7370c214cae9acab5401cd0674b8b4
Geonwoo Moon <moongunwoo7019@gmail.com>
2024-06-30 04:12:13 +0900

    fix: urls name 수정

--- diagnosis/urls.py
+++ diagnosis/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "diagnosis"
 
 urlpatterns = [
-    path("", views.Diagnosis.as_view(), name="diagnosis_list"),
+    path("", views.Diagnosis.as_view(), name="start_diagnose"),
     path("<int:pk>/", views.DiagnoseDetail.as_view(), name="diagnose_detail"),
     path("history/", views.DiagnoseHistory.as_view(), name="diagnose_history"),
 ]
--- users/urls.py
+++ users/urls.py
@@ -7,8 +7,8 @@ from rest_framework_simplejwt.views import (
 from .views import Users, Me
 
 urlpatterns = [
-    path("", Users.as_view(), name="users_list"),
-    path("me/", Me.as_view(), name="user_detail"),
+    path("", Users.as_view(), name="signup"),
+    path("me/", Me.as_view(), name="me"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
