190673a04c5dbe3c026b7f0da34bbb85be574feb
Geon-Woo <moongunwoo7019@naver.com>
2024-07-09 23:16:42 +0900

    fix: receipt, past_medicine owner_name > owner

--- receipts/admin.py
+++ receipts/admin.py
@@ -4,23 +4,23 @@ from .models import Receipt, PastMedicine
 
 @admin.register(Receipt)
 class ReceiptAdmin(admin.ModelAdmin):
-    def owner_name(self, receipt):
-        return receipt.owner.name
+    def owner(self, receipt):
+        return receipt.owner
 
     list_display = (
-        "owner_name",
+        "owner",
         "purchase_at",
     )
 
 
 @admin.register(PastMedicine)
 class PastMedicineAdmin(admin.ModelAdmin):
-    def owner_name(self, past_medicine):
+    def owner(self, past_medicine):
         return past_medicine.receipt.owner.name
 
     list_display = (
         "medicine",
-        "owner_name",
+        "owner",
         "price_per_medicine_at_purchase",
     )
 
